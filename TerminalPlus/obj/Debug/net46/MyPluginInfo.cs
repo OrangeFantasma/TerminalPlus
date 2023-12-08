72380d28d21d122385c29e04b675c8e5ce6bddf1
OrangeFantasma <gavin.borthwick@yahoo.com>
2023-12-07 22:10:51 -0500

    state

--- TerminalPlus/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TerminalPlus
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "TerminalPlus";
-        public const string PLUGIN_NAME = "TerminalPlus";
-        public const string PLUGIN_VERSION = "1.0.4";
-    }
-}
